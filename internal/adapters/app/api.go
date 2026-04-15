7f6575f06332e0e6aba0771f278e9ca0cc60a640
adotkaya <adotkaya@gmail.com>
2026-04-15 17:42:34 +0300

    I HATE HATE HATE

--- internal/adapters/app/api.go
+++ internal/adapters/app/api.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"fmt"
 	"go-hex/internal/ports"
 )
 
@@ -13,17 +14,37 @@ func NewAdapter(arith ports.ArithmeticPort) *Adapter {
 }
 
 func (apiar *Adapter) GetAddition(a, b int32) (int32, error) {
-	return apiar.arith.Addition(a, b)
+	answer, err := apiar.arith.Addition(a, b)
+	if err != nil {
+		fmt.Println(err)
+		return 0, err
+	}
+	return answer, nil
 }
 
 func (apiar *Adapter) GetSubstraction(a, b int32) (int32, error) {
-	return apiar.arith.Substraction(a, b)
+	answer, err := apiar.arith.Substraction(a, b)
+	if err != nil {
+		fmt.Println(err)
+		return 0, err
+	}
+	return answer, nil
 }
 
 func (apiar *Adapter) GetMultiplication(a, b int32) (int32, error) {
-	return apiar.arith.Multiplication(a, b)
+	answer, err := apiar.arith.Multiplication(a, b)
+	if err != nil {
+		fmt.Println(err)
+		return 0, err
+	}
+	return answer, nil
 }
 
 func (apiar *Adapter) GetDivision(a, b int32) (int32, error) {
-	return apiar.arith.Division(a, b)
+	answer, err := apiar.arith.Division(a, b)
+	if err != nil {
+		fmt.Println(err)
+		return 0, err
+	}
+	return answer, nil
 }
